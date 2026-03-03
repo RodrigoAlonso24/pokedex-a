456511e43744ee16187407e00b8cdca7996d1aba
Rodrigo <up240232@alumnos.upa.edu.mx>
2026-03-02 20:10:27 -0600

    add fetch getPokemons

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,25 @@
+import React, { useEffect } from "react";
 import { Text, View } from "react-native";
 
 export default function Index() {
+  useEffect(() => {
+    console.log("entre a pantalla");
+    getPokemons();
+  }, []);
+
+  const getPokemons = async () => {
+    const URL = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0";
+    const response = await fetch(URL, {
+      method: "GET",
+    });
+    console.log(response);
+    const data = await response.json();
+    console.log(data);
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View>
+      <Text>Rodrigo</Text>
     </View>
   );
 }
